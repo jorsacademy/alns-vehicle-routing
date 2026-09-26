02f71bee9c7b5cf9a6bf210f0736342cbaef81f7
Jors Academy <abalci16@posta.pau.edu.tr>
2026-09-26 21:31:55 +0300

    Fix Ruff issues in projects/rl-hyper-heuristic-cvrp/src/rlhh_cvrp/problem.py

--- projects/rl-hyper-heuristic-cvrp/src/rlhh_cvrp/problem.py
+++ projects/rl-hyper-heuristic-cvrp/src/rlhh_cvrp/problem.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
+from collections.abc import Iterable
 from dataclasses import dataclass
 from math import ceil
-from typing import Iterable
 
 import numpy as np
 
